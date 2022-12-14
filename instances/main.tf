ce019f42a0d0f27d94a543bfc9646a9d8e171484
aamdevsecops <99119220+aamdevsecops@users.noreply.github.com>
2022-12-14 21:30:21 +0100

    Update main.tf

--- instances/main.tf
+++ instances/main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "vm_instance" {
   name = "${var.instance_name}"
-  # RESOURCE properties go here
+  # Define resource properties
   zone         = "${var.instance_zone}"
   machine_type = "${var.instance_type}"
   boot_disk {
